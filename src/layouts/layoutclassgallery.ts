e095fecf13fca9bda3f623ddcc69ff2f560c288a
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-10-16 16:35:37 -0400

    super hacky implementation of #2905: marking selected image in gallery. This is NOT declarative!

--- src/layouts/layoutclassgallery.ts
+++ src/layouts/layoutclassgallery.ts
@@ -8,10 +8,46 @@ export class UIGalleryLayoutClass extends UIListLayoutClass {
     renderElementLabels = false
     _sortOrder: Array<string> = []
 
+    // @state()
+    selectedItem: string | undefined;
+
     // @ts-ignore
     renderLayoutStyles(layout?: UISchemaUIElementLayoutSettings): string {
         let style = ""
         return style
+    }
+
+    public itemClicked(e: MouseEvent) {
+        // todo: refactor
+        // this is a super hacky solution because it undermines the declarative architecture
+        // but because right now there is no "store" that all elements could use
+        // this is the best I can think of right now.
+
+        if (e.target instanceof HTMLElement) {
+            if (e.target.tagName === "FILE-VIEW") {
+                e.stopPropagation()
+                let parent: HTMLElement | null = e.target.parentElement
+                let gallery: HTMLElement |undefined = undefined
+                let selectedItem: HTMLElement |undefined = undefined
+                while (parent) {
+                   if (parent.classList.contains("gallery-item")) selectedItem = parent;
+                   if (parent.classList.contains("ui-gallery-layout")) {
+                       gallery = parent;
+                       break;
+                   }
+                   if (parent.tagName === "ui-component") {
+                       parent = null
+                   } else {
+                       parent = parent.parentElement
+                   }
+                }
+                if (selectedItem && gallery)  {
+                    let elements = gallery.querySelectorAll(".gallery-item-selected")
+                    elements.forEach(e => {console.log(e); e.classList.remove("gallery-item-selected")})
+                    selectedItem.classList.add("gallery-item-selected")
+                }
+            }
+        }
     }
 
     public renderLayout(renderContext: UILayoutRenderContext, layouter: UILayoutClass, style: string,
@@ -23,7 +59,7 @@ export class UIGalleryLayoutClass extends UIListLayoutClass {
         this._initSorting(renderContext)
         while (renderContext.next()) {
             items.push(html`
-                <div id="R${renderContext.getCurrentUID()?renderContext.getCurrentUID():nothing}" class="gallery-item">
+                <div id="R${renderContext.getCurrentUID()?renderContext.getCurrentUID():nothing}" class="gallery-item " @click="${this.itemClicked}">
                     ${elements.map((id) => renderElement(id, renderContext.entry.ui_elements[id], layouter))}
                 </div>
             `)
--- types/layouts/layoutclassgallery.d.ts
+++ types/layouts/layoutclassgallery.d.ts
@@ -6,7 +6,9 @@ export declare class UIGalleryLayoutClass extends UIListLayoutClass {
     cssClass: string;
     renderElementLabels: boolean;
     _sortOrder: Array<string>;
+    selectedItem: string | undefined;
     renderLayoutStyles(layout?: UISchemaUIElementLayoutSettings): string;
+    itemClicked(e: MouseEvent): void;
     renderLayout(renderContext: UILayoutRenderContext, layouter: UILayoutClass, style: string, renderElement: (id: string, entry: UISchemaUIElement, layouter: UILayoutClass) => TemplateResult): TemplateResult;
     renderElement(layout: UISchemaUIElementLayoutSettings | undefined, element: TemplateResult): TemplateResult;
 }
